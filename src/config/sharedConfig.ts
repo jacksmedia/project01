7c54fec9d3dda75f23b772c3fe0008497ddc8cba
jacksmedia <alex@j4cks.com>
2023-11-20 22:46:59 -0800

    🗝️ adds walletconnect dapp ID

--- src/config/sharedConfig.ts
+++ src/config/sharedConfig.ts
@@ -1,5 +1,4 @@
-// Generate your own WalletConnect 2 ProjectId here: https://cloud.walletconnect.com/app
-export const walletConnectV2ProjectId = '9b1a9564f91cb659ffe21b73d5c4e2d8';
+export const walletConnectV2ProjectId = 'd54539c88deb228823b6789a19746e0b';
 export const apiTimeout = 6000;
 export const transactionSize = 10;
 export const nativeAuth = true;
